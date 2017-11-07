c08174d6af9fef69c884668f0c9006efecc20b58
Sam Sartor <me@samsartor.com>
2017-11-06 20:45:17 -0700

    readme & licence

--- scripts/upload-doc.sh
+++ scripts/upload-doc.sh
@@ -2,7 +2,7 @@
 set -ex
 
 cargo doc
-echo "<meta http-equiv=refresh content=0;url=flight/index.html>" > target/doc/index.html
+echo "<meta http-equiv=refresh content=0;url=picea/index.html>" > target/doc/index.html
 wget "https://img.shields.io/badge/doc-`git rev-parse --short HEAD`-blue.svg?style=flat-square" -O target/doc/doc_shield.svg
 ghp-import -n target/doc
 git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
